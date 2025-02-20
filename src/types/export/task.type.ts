2a52aef9cb6de659cd68776192072b8cb21f7b7b
RonitKissis <ronitkisis1997@gmail.com>
2025-02-20 10:27:34 +0200

    fix: remove task params

--- src/types/export/task.type.ts
+++
@@ -1,4 +0,0 @@
-import z from 'zod';
-import { exportTaskParametersSchema } from '../../schemas/export/task.schema';
-
-export type ExportTaskParameters = z.infer<typeof exportTaskParametersSchema>;
