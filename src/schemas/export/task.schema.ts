7e7a673e2e898fa5e7b14d4c79f4243db4de57bc
almog8k <almog8k@gmail.com>
2025-05-14 09:53:49 +0300

    refactor: remove status discriminatedUnion from exportFinalizeTaskParamsSchema

--- src/schemas/export/task.schema.ts
+++ src/schemas/export/task.schema.ts
@@ -1,19 +1,9 @@
-import { OperationStatus } from '@map-colonies/mc-priority-queue';
 import z from 'zod';
 
 export const exportFinalizeTaskParamsSchema = z
-  .discriminatedUnion('status', [
-    // Schema when status is Failed
-    z.object({
-      status: z.literal(OperationStatus.FAILED),
-      callbacksSent: z.boolean(),
-    }),
-    // Schema when status is Completed
-    z.object({
-      status: z.literal(OperationStatus.COMPLETED),
-      gpkgModified: z.boolean(),
-      gpkgUploadedToS3: z.boolean(),
-      callbacksSent: z.boolean(),
-    }),
-  ])
+  .object({
+    gpkgModified: z.boolean(),
+    gpkgUploadedToS3: z.boolean(),
+    callbacksSent: z.boolean(),
+  })
   .describe('exportFinalizeTaskParamsSchema');
