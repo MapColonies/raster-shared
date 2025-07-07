bd01f263519cf026537fda8c96ac56d91d973bb4
almog8k <almog8k@gmail.com>
2025-07-07 10:37:26 +0300

    refactor: refactor: insert shp file-path to input files for  improved structure

--- src/schemas/ingestion/inputFiles.schema.ts
+++ src/schemas/ingestion/inputFiles.schema.ts
@@ -1,15 +1,29 @@
 import { z } from 'zod';
 import { INGESTION_VALIDATIONS } from '../../constants/ingestion/constants';
 
-export const inputFilesSchema = z
-  .object({
-    originDirectory: z.string().min(1, { message: 'Origin directory is required, files should be stored on specific directory' }),
-    fileNames: z
-      .array(z.string().regex(new RegExp(INGESTION_VALIDATIONS.fileNames.pattern), 'File name must end with .gpkg'))
-      .length(1, { message: 'Number of files should be 1' }),
-  })
-  .describe('inputFilesSchema');
+export const gpkgFileNameSchema = z
+  .string()
+  .regex(new RegExp(INGESTION_VALIDATIONS.fileNames.pattern), 'File name must end with .gpkg')
+  .describe('gpkgFileNameSchema');
 
 export const shapefilePathSchema = z
   .string()
   .regex(new RegExp(INGESTION_VALIDATIONS.shapefileFilePath.pattern), 'File path must be a valid shapefile path ending with .shp');
+
+export const filesSchema = z
+  .object({
+    gpkgFiles: z
+      .array(gpkgFileNameSchema, {
+        message: 'Files should be an array of .gpkg file names',
+      })
+      .length(1, { message: 'Number of files should be 1' }),
+    shapeFilePath: shapefilePathSchema,
+  })
+  .describe('filesSchema');
+
+export const inputFilesSchema = z
+  .object({
+    originDirectory: z.string().min(1, { message: 'Origin directory is required, files should be stored on specific directory' }),
+    files: filesSchema,
+  })
+  .describe('inputFilesSchema');
