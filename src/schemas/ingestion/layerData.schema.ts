8ff62f8c1a473b70100f1d48009274ebdb0834fa
shlomiko <shlomiko@rafael.co.il>
2025-09-09 11:40:59 +0300

    feat: added missing resulotion/accuracy field + removed duplicated LayerData type & schema

--- src/schemas/ingestion/layerData.schema.ts
+++
@@ -1,6 +0,0 @@
-import z from 'zod';
-import { inputFilesSchema } from './inputFiles.schema';
-
-export const layerDataSchema = z.object({
-  inputFiles: inputFilesSchema,
-});
--- src/types/ingestion/layer.type.ts
+++ src/types/ingestion/layer.type.ts
@@ -1,11 +1,9 @@
 import z from 'zod';
 import { inputFilesSchema } from '../../schemas/ingestion/inputFiles.schema';
 import { newRasterLayerMetadataSchema, updateRasterLayerMetadataSchema } from '../../schemas/ingestion/metadata.schema';
-import { layerDataSchema } from '../../schemas/ingestion/layerData.schema';
 
 //#region LayerData
 export type InputFiles = z.infer<typeof inputFilesSchema>;
-export type LayerData = z.infer<typeof layerDataSchema>;
 export type NewRasterLayerMetadata = z.infer<typeof newRasterLayerMetadataSchema>;
 export type UpdateRasterLayerMetadata = z.infer<typeof updateRasterLayerMetadataSchema>;
 // #endregion
