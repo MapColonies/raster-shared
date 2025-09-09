1619f732f128751c1de78c3c1b97c0d4cac14f14
almog8k <almog8k@gmail.com>
2025-09-09 11:53:24 +0300

    feat: feat: add validation schema for shapefile feature properties

--- src/schemas/ingestion/shapefile.schema.ts
+++ src/schemas/ingestion/shapefile.schema.ts
@@ -6,6 +6,57 @@ export const shapefileFeaturePropertiesSchema = z
     sourceId: z.string({ message: 'Source id should be a string' }).optional(),
     sourceName: z.string({ message: 'Source name should be a string' }).min(1, { message: 'Source name should have length of at least 1' }),
     desc: z.string({ message: 'Description should be a string' }).optional(),
+    updateDate: z.coerce.date({ message: 'Imaging time begin UTC should be a datetime' }),
+    publishRes: z
+      .number({ message: 'Resolution meter should be a number' })
+      .min(INGESTION_VALIDATIONS.resolutionMeter.min, {
+        message: `Resolution meter should not be less than ${INGESTION_VALIDATIONS.resolutionMeter.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.resolutionMeter.max, {
+        message: `Resolution meter should not be larger than ${INGESTION_VALIDATIONS.resolutionMeter.max}`,
+      }),
+    sourceRes: z
+      .number({ message: 'Source resolution meter should be a number' })
+      .min(INGESTION_VALIDATIONS.resolutionMeter.min, {
+        message: `Source resolution meter should not be less than ${INGESTION_VALIDATIONS.resolutionMeter.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.resolutionMeter.max, {
+        message: `Source resolution meter should not be larger than ${INGESTION_VALIDATIONS.resolutionMeter.max}`,
+      }),
+    ep90: z
+      .number({ message: 'Horizontal accuracy CE90 should be a number' })
+      .min(INGESTION_VALIDATIONS.horizontalAccuracyCE90.min, {
+        message: `Horizontal accuracy CE90 should not be less than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.horizontalAccuracyCE90.max, {
+        message: `Horizontal accuracy CE90 should not be larger than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.max}`,
+      }),
+    sensors: z
+      .array(
+        z.string({ message: 'Sensors should be an array of strings' }).regex(new RegExp(INGESTION_VALIDATIONS.sensor.pattern), {
+          message: 'Sensors should be an array with items not starting or ending with whitespace characters',
+        }),
+        { message: 'Sensors should be an array' }
+      )
+      .min(1, { message: 'Sensors should have an array length of at least 1' }),
+    countries: z
+      .array(z.string({ message: 'Countries should be an array of strings' }).min(1, { message: 'Countries should have length of at least 1' }), {
+        message: 'Countries should be an array',
+      })
+      .optional(),
+    cities: z
+      .array(z.string({ message: 'Cities should be an array of strings' }).min(1, { message: 'Cities should have length of at least 1' }), {
+        message: 'Cities should be an array',
+      })
+      .optional(),
+  })
+  .describe('partSchema');
+
+export const layerPartsFeaturePropertiesSchema = z
+  .object({
+    sourceId: z.string({ message: 'Source id should be a string' }).optional(),
+    sourceName: z.string({ message: 'Source name should be a string' }).min(1, { message: 'Source name should have length of at least 1' }),
+    desc: z.string({ message: 'Description should be a string' }).optional(),
     timeBegin: z.coerce.date({ message: 'Imaging time begin UTC should be a datetime' }),
     timeEnd: z.coerce.date({ message: 'Imaging time end UTC should be a datetime' }),
     resDegree: z
