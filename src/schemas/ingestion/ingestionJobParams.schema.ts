65ff0b3d4fd98d21728007389bbe8a57f366d4f9
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-09-16 16:31:29 +0300

    feat!: ingestion resolution schema

--- src/schemas/ingestion/ingestionJobParams.schema.ts
+++ src/schemas/ingestion/ingestionJobParams.schema.ts
@@ -1,13 +1,24 @@
 import z from 'zod';
+import { CORE_VALIDATIONS } from '../../constants';
 import { callbackUrlsArraySchema } from '../core/callbackUrl.schema';
 import { newAdditionalParamsSchema, swapUpdateAdditionalParamsSchema, updateAdditionalParamsSchema } from './additionalParams.schema';
 import { inputFilesSchema } from './inputFiles.schema';
 import { newRasterLayerMetadataSchema, updateRasterLayerMetadataSchema } from './metadata.schema';
 
+export const ingestionResolutionSchema = z
+  .number({ message: 'Resolution degree should be a number' })
+  .min(CORE_VALIDATIONS.resolutionDeg.min, {
+    message: `Resolution degree should not be less than ${CORE_VALIDATIONS.resolutionDeg.min}`,
+  })
+  .max(CORE_VALIDATIONS.resolutionDeg.max, {
+    message: `Resolution degree should not be larger than ${CORE_VALIDATIONS.resolutionDeg.max}`,
+  });
+
 export const ingestionNewJobParamsSchema = z
   .object({
     metadata: newRasterLayerMetadataSchema,
     inputFiles: inputFilesSchema,
+    ingestionResolution: ingestionResolutionSchema,
     callbackUrls: callbackUrlsArraySchema.optional(),
     additionalParams: newAdditionalParamsSchema,
   })
@@ -17,6 +28,7 @@ export const ingestionUpdateJobParamsSchema = z
   .object({
     metadata: updateRasterLayerMetadataSchema,
     inputFiles: inputFilesSchema,
+    ingestionResolution: ingestionResolutionSchema,
     callbackUrls: callbackUrlsArraySchema.optional(),
     additionalParams: updateAdditionalParamsSchema,
   })
@@ -26,6 +38,7 @@ export const ingestionSwapUpdateJobParamsSchema = z
   .object({
     metadata: updateRasterLayerMetadataSchema,
     inputFiles: inputFilesSchema,
+    ingestionResolution: ingestionResolutionSchema,
     callbackUrls: callbackUrlsArraySchema.optional(),
     additionalParams: swapUpdateAdditionalParamsSchema,
   })
