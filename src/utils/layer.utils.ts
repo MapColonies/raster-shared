3eab68ca90b1aeb82295c1193cfbb1fd7fc43ac1
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-09-21 16:21:42 +0300

    refactor: pr comments

--- src/utils/layer.utils.ts
+++ src/utils/layer.utils.ts
@@ -1,13 +1,15 @@
 import { RasterProductTypes } from '../constants';
-import { polygonPartsEntityPatternSchema } from '../schemas';
-import type { PolygonPartsEntityName, LayerName } from '../types';
+import { polygonPartsEntityPatternSchema, resourceIdSchema } from '../schemas';
+import type { LayerName, PolygonPartsEntityName } from '../types';
 
 export function getEntityName(productId: string, productType: RasterProductTypes): PolygonPartsEntityName {
+  resourceIdSchema.parse(productId);
   const entityName = [productId, productType].join('_').toLowerCase();
   return polygonPartsEntityPatternSchema.parse(entityName);
 }
 
 export function getMapServingLayerName(productId: string, productType: RasterProductTypes): LayerName {
+  resourceIdSchema.parse(productId);
   const layerName = `${productId}-${productType}` satisfies LayerName;
   return layerName;
 }
