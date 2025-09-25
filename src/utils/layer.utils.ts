4aa1d400e1554a370ae3c9e6e987aa84de0dce41
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-09-25 11:46:46 +0300

    feat!: map layer name (#116)

--- src/utils/layer.utils.ts
+++ src/utils/layer.utils.ts
@@ -1,8 +1,15 @@
 import { RasterProductTypes } from '../constants';
-import { polygonPartsEntityPatternSchema } from '../schemas';
-import { PolygonPartsEntityName } from '../types';
+import { polygonPartsEntityPatternSchema, resourceIdSchema } from '../schemas';
+import type { LayerName, PolygonPartsEntityName } from '../types';
 
-export function generateEntityName(productId: string, productType: RasterProductTypes): PolygonPartsEntityName {
+export function getEntityName(productId: string, productType: RasterProductTypes): PolygonPartsEntityName {
+  resourceIdSchema.parse(productId);
   const entityName = [productId, productType].join('_').toLowerCase();
   return polygonPartsEntityPatternSchema.parse(entityName);
+}
+
+export function getMapServingLayerName(productId: string, productType: RasterProductTypes): LayerName {
+  resourceIdSchema.parse(productId);
+  const layerName = `${productId}-${productType}` satisfies LayerName;
+  return layerName;
 }
