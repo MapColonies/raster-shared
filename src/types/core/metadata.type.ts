27bd4a1afdccf005d866b9205b4b201c6d50fa5e
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-10-16 23:57:21 +0300

    refactor: move type to other file

--- src/types/core/metadata.type.ts
+++ src/types/core/metadata.type.ts
@@ -1,6 +1,4 @@
 import z from 'zod';
 import type { aggregationFeatureSchema } from '../../schemas';
-import type { rasterLayerCatalogSchema } from '../../schemas/core/layer.schema';
 
-export type RasterLayerCatalog = z.infer<typeof rasterLayerCatalogSchema>;
 export type AggregationFeature = z.infer<typeof aggregationFeatureSchema>;
