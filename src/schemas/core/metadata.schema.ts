ce9b6d596ec40b889cc2767f6d811720b816d0bb
vitaligi <54726763+vitaligi@users.noreply.github.com>
2025-10-16 15:22:06 +0300

    feat: raster layer metadata schema

---
+++ src/schemas/core/metadata.schema.ts
@@ -0,0 +1,110 @@
+import { RecordStatus, RecordType } from '@map-colonies/types';
+import z from 'zod';
+import { CORE_VALIDATIONS, INGESTION_VALIDATIONS, TileOutputFormat, Transparency } from '../../constants';
+import { multiPolygonSchema, polygonSchema } from './geo.schema';
+import { rasterProductTypeSchema, resourceIdSchema, versionSchema } from './job.schema';
+import { tilesMimeFormatSchema } from './mime.schema';
+
+export const rasterLayerMetadataSchema = z.object({
+  id: z.string().uuid(),
+  metadata: z.object({
+    id: z.string().uuid(),
+    srs: z.literal('4326'),
+    productVersion: versionSchema,
+    maxResolutionDeg: z
+      .number({ message: 'Max resolution degree should be a number' })
+      .min(CORE_VALIDATIONS.resolutionDeg.min, {
+        message: `Max resolution degree should not be less than ${CORE_VALIDATIONS.resolutionDeg.min}`,
+      })
+      .max(CORE_VALIDATIONS.resolutionDeg.max, {
+        message: `Max resolution degree should not be larger than ${CORE_VALIDATIONS.resolutionDeg.max}`,
+      }),
+    minResolutionDeg: z
+      .number({ message: 'Min resolution degree should be a number' })
+      .min(CORE_VALIDATIONS.resolutionDeg.min, {
+        message: `Min resolution degree should not be less than ${CORE_VALIDATIONS.resolutionDeg.min}`,
+      })
+      .max(CORE_VALIDATIONS.resolutionDeg.max, {
+        message: `Min resolution degree should not be larger than ${CORE_VALIDATIONS.resolutionDeg.max}`,
+      }),
+    scale: z.number().min(INGESTION_VALIDATIONS.scale.min).max(INGESTION_VALIDATIONS.scale.max).optional(),
+    creationDateUTC: z.coerce.date().optional(),
+    ingestionDate: z.coerce.date().optional(),
+    minHorizontalAccuracyCE90: z
+      .number({ message: 'Min horizontal accuracy CE90 should be a number' })
+      .min(INGESTION_VALIDATIONS.horizontalAccuracyCE90.min, {
+        message: `Min horizontal accuracy CE90 should not be less than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.horizontalAccuracyCE90.max, {
+        message: `Min horizontal accuracy CE90 should not be larger than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.max}`,
+      }),
+    maxHorizontalAccuracyCE90: z
+      .number({ message: 'Max horizontal accuracy CE90 should be a number' })
+      .min(INGESTION_VALIDATIONS.horizontalAccuracyCE90.min, {
+        message: `Max horizontal accuracy CE90 should not be less than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.horizontalAccuracyCE90.max, {
+        message: `Max horizontal accuracy CE90 should not be larger than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.max}`,
+      }),
+    region: z.array(z.string().min(1)).min(1),
+    sensors: z
+      .array(
+        z.string({ message: 'Sensors should be an array of strings' }).regex(new RegExp(INGESTION_VALIDATIONS.sensor.pattern), {
+          message: 'Sensors should be an array with items not starting or ending with whitespace characters',
+        }),
+        { message: 'Sensors should be an array' }
+      )
+      .min(1, { message: 'Sensors should have an array length of at least 1' }),
+    imagingTimeBeginUTC: z.coerce.date({ message: 'Imaging time begin UTC should be a datetime' }),
+    imagingTimeEndUTC: z.coerce.date({ message: 'Imaging time end UTC should be a datetime' }),
+    updateDateUTC: z.coerce.date().optional(),
+    maxResolutionMeter: z
+      .number({ message: 'Max resolution meter should be a number' })
+      .min(INGESTION_VALIDATIONS.resolutionMeter.min, {
+        message: `Max resolution meter should not be less than ${INGESTION_VALIDATIONS.resolutionMeter.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.resolutionMeter.max, {
+        message: `Max resolution meter should not be larger than ${INGESTION_VALIDATIONS.resolutionMeter.max}`,
+      }),
+    minResolutionMeter: z
+      .number({ message: 'Min horizontal accuracy CE90 should be a number' })
+      .min(INGESTION_VALIDATIONS.horizontalAccuracyCE90.min, {
+        message: `Min horizontal accuracy CE90 should not be less than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.min}`,
+      })
+      .max(INGESTION_VALIDATIONS.horizontalAccuracyCE90.max, {
+        message: `Min horizontal accuracy CE90 should not be larger than ${INGESTION_VALIDATIONS.horizontalAccuracyCE90.max}`,
+      }),
+    productSubType: z.string().optional(),
+    productBoundingBox: z
+      .string({ message: 'Product bounding box should be a string' })
+      .regex(new RegExp(INGESTION_VALIDATIONS.boundingBox.pattern), {
+        message: 'Product bounding box must be of the shape min_x,min_y,max_x,max_y',
+      }),
+    displayPath: z.string().uuid(),
+    transparency: z.nativeEnum(Transparency),
+    tileMimeFormat: tilesMimeFormatSchema,
+    tileOutputFormat: z.nativeEnum(TileOutputFormat),
+    productStatus: z.nativeEnum(RecordStatus).optional(),
+    type: z.literal(RecordType.RECORD_RASTER),
+    classification: z
+      .string()
+      .regex(new RegExp(INGESTION_VALIDATIONS.classification.pattern), { message: 'Classification value must be between 0 and 100' }),
+    productName: z.string(),
+    description: z.string(),
+    srsName: z.literal('WGS84GEO'),
+    producerName: z.string(),
+    footprint: polygonSchema.or(multiPolygonSchema),
+    productId: resourceIdSchema,
+    productType: rasterProductTypeSchema,
+  }),
+  links: z
+    .array(
+      z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        protocol: z.string().optional(),
+        url: z.string().optional(),
+      })
+    )
+    .optional(),
+});
--- src/types/core/metadata.type.ts
+++ src/types/core/metadata.type.ts
@@ -1,34 +1,6 @@
 import z from 'zod';
-import { IMetadataCommonModel, RecordStatus, TilesMimeFormat } from '@map-colonies/types';
-import { TileOutputFormat, Transparency } from '../../constants/core';
 import { aggregationFeatureSchema } from '../../schemas';
+import type { rasterLayerMetadataSchema } from '../../schemas/core/metadata.schema';
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-export type RasterLayerMetadata = {
-  id: string;
-  srs: string;
-  productVersion: string;
-  maxResolutionDeg: number;
-  minResolutionDeg: number;
-  scale?: number;
-  creationDateUTC?: Date;
-  ingestionDate?: Date;
-  minHorizontalAccuracyCE90: number;
-  maxHorizontalAccuracyCE90: number;
-  region: string[];
-  sensors: string[];
-  imagingTimeBeginUTC: Date;
-  imagingTimeEndUTC: Date;
-  updateDateUTC?: Date;
-  maxResolutionMeter: number;
-  minResolutionMeter: number;
-  productSubType?: string;
-  productBoundingBox?: string;
-  displayPath: string;
-  transparency: Transparency;
-  tileMimeFormat: TilesMimeFormat;
-  tileOutputFormat: TileOutputFormat;
-  productStatus?: RecordStatus;
-} & IMetadataCommonModel;
-
+export type RasterLayerMetadata = z.infer<typeof rasterLayerMetadataSchema>;
 export type AggregationFeature = z.infer<typeof aggregationFeatureSchema>;
