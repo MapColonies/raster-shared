a4ec62c00cc65c97a7250e17bf87f340424d0038
shlomiko <shlomiko@rafael.co.il>
2025-10-30 11:15:08 +0200

    fix: changed gpkg file path regex pattern

--- src/constants/ingestion/constants.ts
+++ src/constants/ingestion/constants.ts
@@ -33,7 +33,7 @@ export const INGESTION_VALIDATIONS = {
     description: 'Classification value between 0 and 100',
   },
   gpkgfileName: {
-    pattern: '^((\\/[\\w-]+)+)\\/([^\\/][\\wא-ת\\.-]+)\\.gpkg$',
+    pattern: '^(\\/?[\\w-]+)(\\/[\\w-]+)*\\/[\\wא-ת\.-]+\\.gpkg$',
     description: 'Valid gpkg file path, file name must end with .gpkg',
   },
   metadataShapefilePath: {
