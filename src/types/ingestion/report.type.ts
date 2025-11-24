9db254e9d50dc2714b02fc33e03a242ec2871b08
almog8k <almog8k@gmail.com>
2025-11-24 13:46:52 +0200

    feat: add validation error types and update report type to use them

--- src/types/ingestion/report.type.ts
+++ src/types/ingestion/report.type.ts
@@ -1,6 +1,13 @@
+import { ValidationErrorType } from '../../constants';
+
+export type PolygonPartValidationErrorsType = Pick<
+  typeof ValidationErrorType,
+  'RESOLUTION' | 'GEOMETRY_VALIDITY' | 'SMALL_GEOMETRY' | 'SMALL_HOLES'
+>[keyof Pick<typeof ValidationErrorType, 'GEOMETRY_VALIDITY' | 'RESOLUTION' | 'SMALL_GEOMETRY' | 'SMALL_HOLES'>];
+
 export interface PolygonPartValidationError {
   id: string;
-  errors: string[];
+  errors: PolygonPartValidationErrorsType[];
 }
 export interface PolygonPartsChunkValidationResult {
   parts: PolygonPartValidationError[];
