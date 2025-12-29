2574bcd364710729399c2856328146ee9093b9f9
almog8k <almog8k@gmail.com>
2025-12-29 09:26:50 +0200

    refactor:  update export input parameters to use previous callbackUrlsArraySchema

--- src/schemas/export/job.schema.ts
+++ src/schemas/export/job.schema.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod';
 import { CORE_VALIDATIONS, TileFormatStrategy, TileOutputFormat } from '../../constants';
 import { polygonPartsEntityNameSchema } from '../ingestion';
-import { urlsArraySchema } from '../core';
 import { callbackExportResponseSchema, cleanupDataSchema, roiFeatureCollectionSchema, fileNamesTemplatesSchema } from './export.schema';
+
+export const callbackUrlSchema = z.object({
+  url: z.string().url(),
+});
+
+export const callbackUrlsArraySchema = z.array(callbackUrlSchema); // TODO: remove when all services use the same callback schema( currently ingestion uses urlsArraySchema and export uses callbackUrlSchema)
 
 export const exportInputParamsSchema = z.object({
   crs: z.string(z.literal('EPSG:4326')),
   roi: roiFeatureCollectionSchema,
-  callbackUrls: urlsArraySchema.optional(),
+  callbackUrls: callbackUrlsArraySchema.optional(),
 });
 
 export const exportAdditionalParamsSchema = z
