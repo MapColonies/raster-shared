24ebf182e9f1405a5cbd42ebff982782507c9baf
almog8k <almog8k@gmail.com>
2026-01-04 11:36:16 +0200

    refactor: remove polygonPartsEntityName from baseAdditionalParamsSchema

--- src/schemas/ingestion/additionalParams.schema.ts
+++ src/schemas/ingestion/additionalParams.schema.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
 import { CORE_VALIDATIONS, TileOutputFormat } from '../../constants/core/constants';
-import { polygonPartsEntityPatternSchema } from './layerNameFormats.schema';
 
 export const baseAdditionalParamsSchema = z
   .object({
     jobTrackerServiceURL: z.string().regex(new RegExp(CORE_VALIDATIONS.url.pattern), { message: 'URL must start with http:// or https://' }),
-    polygonPartsEntityName: polygonPartsEntityPatternSchema.optional(),
   })
   .describe('baseAdditionalParamsSchema');
 
