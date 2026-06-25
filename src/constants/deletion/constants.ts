11bd8dc6a2634c2c4298425246879b2f1fdf04a0
almog8k <almog8k@gmail.com>
2026-06-25 15:52:16 +0300

    feat: add layer deletion job and task type constants

---
+++ src/constants/deletion/constants.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export const DeletionJobTypes = {
+  Delete_Layer: 'Delete_Layer',
+} as const;
+
+export type DeletionJobTypes = (typeof DeletionJobTypes)[keyof typeof DeletionJobTypes];
+
+export const DeletionTaskTypes = {
+  Delete: 'delete',
+  LayerTilesDeletion: 'full-layer-tiles-deletion',
+  ArtifactsDeletion: 'artifacts-deletion',
+} as const;
+
+export type DeletionTaskTypes = (typeof DeletionTaskTypes)[keyof typeof DeletionTaskTypes];
+/* eslint-enable @typescript-eslint/naming-convention */
