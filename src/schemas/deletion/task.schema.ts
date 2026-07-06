7d9705e5a2fcd82221c28b10c21720752a3fc8ce
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-07-06 17:42:20 +0300

    fix: remove duplicate export

--- src/schemas/deletion/task.schema.ts
+++ src/schemas/deletion/task.schema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { SourceType } from '../../constants/core/constants';
+import { storageSchema } from '../core';
 
 export const sourceProviderSchema = z.union([z.literal(SourceType.S3), z.literal(SourceType.FS)]);
 
@@ -11,17 +12,6 @@ export const deleteTaskParamsSchema = z
     deletePolygonParts: z.boolean().default(false),
   })
   .describe('deleteTaskParamsSchema');
-
-export const fsStorageSchema = z.object({
-  sourceProvider: z.literal(SourceType.FS),
-});
-
-export const s3StorageSchema = z.object({
-  sourceProvider: z.literal(SourceType.S3),
-  bucket: z.string().min(1),
-});
-
-export const storageSchema = z.discriminatedUnion('sourceProvider', [fsStorageSchema, s3StorageSchema]);
 
 export const deleteStoredResourcesParamsBaseSchema = z.object({
   paths: z.array(z.string().min(1)).min(1),
