b21a83e8d3231a1d892e9e5516db21f9484d1d5a
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-07-07 10:59:57 +0300

    refactor: rename sourceProvider to storageProvider

--- src/schemas/core/storage.schema.ts
+++ src/schemas/core/storage.schema.ts
@@ -2,12 +2,12 @@ import { z } from 'zod';
 import { SourceType } from '../../constants/core/constants';
 
 export const fsStorageSchema = z.object({
-  sourceProvider: z.literal(SourceType.FS),
+  storageProvider: z.literal(SourceType.FS),
 });
 
 export const s3StorageSchema = z.object({
-  sourceProvider: z.literal(SourceType.S3),
+  storageProvider: z.literal(SourceType.S3),
   bucket: z.string().min(1),
 });
 
-export const storageSchema = z.discriminatedUnion('sourceProvider', [fsStorageSchema, s3StorageSchema]);
+export const storageSchema = z.discriminatedUnion('storageProvider', [fsStorageSchema, s3StorageSchema]);
