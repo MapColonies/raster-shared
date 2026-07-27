c832749354c6e4b73fc3cc22cf3584e0ee5daec3
almog8k <60139576+almog8k@users.noreply.github.com>
2026-07-27 11:28:10 +0300

    feat: add subpath field to fsStorageSchema (#216)

--- src/schemas/core/storage.schema.ts
+++ src/schemas/core/storage.schema.ts
@@ -3,6 +3,7 @@ import { SourceType } from '../../constants/core/constants';
 
 export const fsStorageSchema = z.object({
   storageProvider: z.literal(SourceType.FS),
+  subPath: z.string().min(1),
 });
 
 export const s3StorageSchema = z.object({
